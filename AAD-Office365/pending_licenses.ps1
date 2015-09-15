702c5ee8303eb1529978173a5f07b7f18a3ec64a
Jeff Whitworth <jwhitwo@gmail.com>
2015-09-15 15:44:21 -0400

    Add pending_licenses
    Add my existing ps1 that outputs all O365 users with a license in the
    state of pending to a file.

---
+++ AAD-Office365/pending_licenses.ps1
@@ -0,0 +1,33 @@
+﻿#connect to O365
+
+$cred = Get-Credential #get the credential of the account to use
+Connect-MsolService -Credential $cred
+
+#retrieve all users in tenant
+$all_users = Get-MsolUser -all
+
+$pending_lic_list = ""
+
+#iterate through each user and check if there are any pending licenses
+foreach($user in $all_users)
+{
+    foreach($lic in $user.Licenses)
+    {
+        foreach($service in $lic)
+        {
+            foreach($sp in $service)
+            {
+                foreach($plan in $sp.ServiceStatus)
+                {
+                    if($plan.ProvisioningStatus -like 'Pending*')
+                    {
+                        $pending_lic_list += "$($user.UserPrincipalName),$($plan.ServicePlan.ServiceName)`r`n"
+                    }
+                }
+            }
+        }
+    }
+}
+
+#output the settings to a file named pending.csv
+$pending_lic_list | Out-File "pending.csv"
